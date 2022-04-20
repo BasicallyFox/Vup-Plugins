92dec1e9c4f7af7dec5fb6ba614cca8b628c3a81
BasicallyFox <73911698+BasicallyFox@users.noreply.github.com>
2022-04-20 18:29:35 -0300

    tks Davi

---
+++ plugins/kkkkkk.py
@@ -0,0 +1,21 @@
+import asyncio
+import os
+import random
+import re
+import requests
+import wget
+import datetime
+import math
+from cowpy import cow
+from random import randint, choice
+
+from kannax import Message, kannax
+
+
+@kannax.on_cmd("(K)$", about={"header": "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"}, trigger="", allow_via_bot=False)
+async def k_(message: Message):
+    """K"""
+    k = "KKKKKKKKK "
+    for _ in range(4):
+        k = k[:-3] + "KKKKKKKKKKK"
+        await message.try_to_edit(k)
